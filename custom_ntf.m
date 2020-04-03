36acba9c5a4199a8a979b9560050bc4c552b909f
John Bell <johnlb@umich.edu>
2020-04-03 13:16:32 -0400

    removed old files

--- custom_ntf.m
+++
@@ -1,5 +0,0 @@
-%% custom_ntf: loads custom-designed NTF from .mat file
-function [NTF] = custom_ntf(filename)
-	libdir = '~/Documents/MATLAB/tools/dsm/';
-	load([libdir 'custom_ntfs/' filename '.mat']);
-end
